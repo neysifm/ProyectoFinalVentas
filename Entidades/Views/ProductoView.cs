b140580db20f14fdb53343880fcb7ed9ef32bf58
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-08-05 14:56:35 -0400

    Entidades

---
+++ Entidades/Views/ProductoView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Views
+{
+    public class ProductoView
+    {
+        public int IdProducto { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Cantidad { get; set; }
+        public decimal Precio { get; set; }
+        public decimal SubTotal { get; set; }
+
+        public ProductosView()
+        {
+            IdProducto = 0;
+            Descripcion = string.Empty;
+            Cantidad = 0;
+            Precio = 0;
+            SubTotal = 0;
+        }
+
+        public void CalcularTotal()
+        {
+            SubTotal = Precio * Cantidad;
+        }
+    }
+}
---
+++ Entidades/Views/VentaView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Views
+{
+    public class VentaView
+    {
+        public int IdVenta { get; set; }
+        public string Nombre { get; set; }
+        public string Expr1 { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime HastaFecha { get; set; }
+        public decimal Total { get; set; }
+
+        public VentaView()
+        {
+            IdVenta = 0;
+            Nombre = string.Empty;
+            Expr1 = string.Empty;
+            Fecha = DateTime.Now;
+            HastaFecha = DateTime.Now;
+            Total = 0;
+        }
+    }
+}
