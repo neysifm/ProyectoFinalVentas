b3f12137cf4b7dc3f10a9e72c909f0efbe57e475
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-08-05 15:32:56 -0400

    Reportes

---
+++ ProyectoFinal/UI/Consultas/ReportesViewers/ClientesReportsViewers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal.UI.Consultas.ReportesViewers
+{
+    public partial class ClientesReportsViewers : MetroFramework.Forms.MetroForm
+    {
+        public ClientesReportsViewers()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ProyectoFinal/UI/Consultas/ReportesViewers/UsuariosReportsViewer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFinal.UI.Consultas.ReportesViewers
+{
+    public partial class UsuariosReportsViewer : MetroFramework.Forms.MetroForm
+    {
+        public UsuariosReportsViewer()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
